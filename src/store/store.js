f53b1765bc41cb0e2a7ba5048f38ae124bda7627
chasendy3-ops <chasendy3@gmail.com>
2026-06-13 08:24:42 +0700

    Add src/store/store.js

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from '../features/usersSlice'
+import chatsSlice from '../features/chatsSlice'
+
+export const store = configureStore({
+    reducer: {
+        users: userReducer,
+        chats: chatsSlice
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+})
