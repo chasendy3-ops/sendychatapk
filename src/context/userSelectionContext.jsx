6cf6136c41533f06e4ea86538b48d330a862f048
chasendy3-ops <chasendy3@gmail.com>
2026-06-13 08:24:27 +0700

    Add src/context/userSelectionContext.jsx

---
+++ src/context/userSelectionContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export function UserContextProvider({ children }) {
+    const [query, setQuery] = useState("");
+
+    return (
+        <UserContext.Provider value={{ query, setQuery }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
